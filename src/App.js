0abde166c872c7ef33deee1747a45414845c1819
Sushmoy_Rahul <45335164+JUCSERahull337@users.noreply.github.com>
2023-11-05 20:39:26 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PhotoGallery from "./Pages/PhotoGallery/PhotoGallery";
+
+function App() {
+  return (
+   <>
+   <PhotoGallery/>
+   </>
+  );
+}
+
+export default App;
